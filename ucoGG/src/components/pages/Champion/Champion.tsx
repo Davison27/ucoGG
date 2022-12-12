a528497f5235e79c754c1a2b49af2a92f96512ab
Davison27 <davidencinarejo27@gmail.com>
2022-12-12 19:36:15 +0100

    Feat: Lol fonts, lore and tag added

--- ucoGG/src/components/pages/Champion/Champion.tsx
+++ ucoGG/src/components/pages/Champion/Champion.tsx
@@ -3,7 +3,11 @@ import {useEffect, useState} from 'react';
 import Loading from '../../layout/Loading';
 import {
   BackgroundImage,
+  Filter,
+  Lore,
   Name,
+  Tag,
+  TagsSection,
   Title,
   TitleSection,
   Wrapper,
@@ -13,6 +17,8 @@ const Champion = ({route}) => {
   const {championName} = route.params;
   const [name, setName] = useState();
   const [title, setTitle] = useState();
+  const [lore, setLore] = useState();
+  const [tags, setTags] = useState<string[]>();
   const [image, setImage] = useState<string>();
   const [version, setVersion] = useState('12.23.1');
   const [isLoading, setIsLoading] = useState(true);
@@ -29,8 +35,10 @@ const Champion = ({route}) => {
         `http://ddragon.leagueoflegends.com/cdn/${version}/data/en_US/champion/${championName}.json`,
       );
       const championResult = await championResponse.json();
-      setName(championResult.data[championName].name);
+      setName(championResult.data[championName].name.toUpperCase());
       setTitle(championResult.data[championName].title);
+      setLore(championResult.data[championName].lore);
+      setTags(championResult.data[championName].tags);
 
       const imageResponse = await fetch(
         `http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${championName}_0.jpg`,
@@ -45,16 +53,19 @@ const Champion = ({route}) => {
     <>
       {!isLoading ? (
         <>
-          <BackgroundImage
-            style={{width: '100%', height: '100%'}}
-            source={{uri: image}}
-          />
-
+          <BackgroundImage source={{uri: image}} />
+          <Filter />
           <Wrapper>
             <TitleSection>
               <Name>{name}</Name>
               <Title>{title}</Title>
             </TitleSection>
+            <Lore>{lore}</Lore>
+            <TagsSection>
+              {tags?.map(tag => (
+                <Tag>{tag}</Tag>
+              ))}
+            </TagsSection>
           </Wrapper>
         </>
       ) : (
--- ucoGG/src/components/pages/Champion/Champion_styles.ts
+++ ucoGG/src/components/pages/Champion/Champion_styles.ts
@@ -9,15 +9,54 @@ export const BackgroundImage = styled(Image)`
     right: 0;
 `
 
-export const Name = styled(Text)`
-    color: white;
-    font-size: 30px;
-    font-weight: 800;
+export const Filter = styled(View)`
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background-color: black;
+    opacity: 0.6;
 `
 
-export const Title = styled(Text)`
+export const Lore = styled(Text)`
+    font-family: "BeaufortforLOL-Bold";
     color: white;
-    font-size: 15px;
+    background-color: black;
+    border-radius: 10px;
+    margin: 30px 0;
+    padding: 15px;
+    opacity: 0.6;
+`
+
+export const Name = styled(Text)`
+    font-family: "BeaufortforLOL-Bold";
+    color: white;
+    font-size: 50px;
+`
+
+export const Tag= styled(Text)`
+    background-color: black;
+    font-family: "BeaufortforLOL-Bold";
+    color: white;
+    border-radius: 20px;
+    padding: 10px;
+    margin: 0 10px;
+    opacity: 0.6;
+`
+
+export const TagsSection= styled(View)`
+    display: flex;
+    justify-content: center;
+    flex-direction: row;
+
+`
+
+
+export const Title = styled(Text)`
+    font-family: "BeaufortforLOL-MediumItalic";
+    color: white;
+    font-size: 25px;
     font-weight: 600;
 `
 
@@ -28,13 +67,7 @@ export const TitleSection = styled(View)`
 `
 
 export const Wrapper = styled(View)`
+    display: flex;
+    flex-direction: column;
     padding: 20px;
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    background-color: black;
-    border-radius: 5px;
-    opacity: 0.65;
 `
