7db4fe55108d5bf93cbe44cadf7a3aa963d55a65
Davison27 <davidencinarejo27@gmail.com>
2022-12-16 21:18:06 +0100

    Feat: Runterra Map added and navigation tabs

--- ucoGG/App.tsx
+++ ucoGG/App.tsx
@@ -3,24 +3,74 @@ import HomeScreen from './src/components/pages/Homescreen/HomeScreen';
 import Champion from './src/components/pages/Champion/Champion';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {WebView} from 'react-native-webview';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function ItemStackScreen() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Champion"
+        component={Champion}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function HomeStackScreen() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Champion"
+        component={Champion}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function MapStackScreen() {
+  return <WebView source={{uri: `https://map.leagueoflegends.com/es_ES`}} />;
+}
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActiveBackgroundColor: '#13181B',
+          tabBarHideOnKeyboard: true,
+        }}>
+        <Tab.Screen
           name="Home"
-          component={HomeScreen}
+          component={HomeStackScreen}
           options={{headerShown: false}}
         />
-        <Stack.Screen
-          name="Champion"
-          component={Champion}
+        <Tab.Screen
+          name="Item"
+          component={ItemStackScreen}
           options={{headerShown: false}}
         />
-      </Stack.Navigator>
+        <Tab.Screen
+          name="Map"
+          component={MapStackScreen}
+          options={{headerShown: false}}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
